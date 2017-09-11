f252d2d7e1c7dd26dddc1eaf8a7d48267d1d8898
Brent <karmapoint@gmail.com>
2017-09-10 19:33:42 -0700

    set up using jquery as inspiration

--- xx.js
+++ xx.js
@@ -1,4 +1,17 @@
 (function(global, $ ){
-  
+  var Greetr = function (firstName, lastName, language){
+    return new xx.init(firstName, lastName, language);
+  };
+
+  Greetr.prototype = {};
+
+  Greetr.init = function(firstName, lastName, language) {
+    this.firstName = firstName || "";
+    this.lastName = lastName || "";
+    this.language = language || "en";
+  };
+
+  Greetr.init.prototype = Greetr.prototype;
+  global.Greetr = global.XX = Greetr;
 
 }(window, jQuery));
